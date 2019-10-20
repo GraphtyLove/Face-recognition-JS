795b334a5131631f2e0143356a984bdad9d5423e
maxim Berge <maxim.berge@gmail.com>
2019-10-20 18:10:16 +0200

    resolve webcam compatibility + comment code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,69 @@
+// Select the video element from HTML
 const video = document.getElementById('video')
 
+// Turn on the webcam of the user and append the video stream to our video html element
 const startVideo = () => {
     navigator.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
-        err => console.log(err)
+        err => console.error(err)
     )
+        || navigator.webkitGetUserMedia(
+            { video: {} },
+            stream => video.srcObject = stream,
+            err => console.error(err)
+        )
+        || navigator.mozGetUserMedia(
+            { video: {} },
+            stream => video.srcObject = stream,
+            err => console.error(err)
+        )
+        || navigator.msGetUserMedia(
+            { video: {} },
+            stream => video.srcObject = stream,
+            err => console.error(err)
+        )
 }
 
+// Load all our models asynchronously, if all our Promises are resolved, run the startVideo function
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/assets/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/assets/models'),
-]).then(startVideo)
+]).then(startVideo).catch(e => console.error(e))
 
 
-
+// Add an event listener when the webcam is steaming
 video.addEventListener('play', () => {
+    // Create a canvas
     const canvas = faceapi.createCanvasFromMedia(video)
+    // Append the canvas to our "mainContainer" div
     document.getElementById('main').append(canvas)
+    // Save the size of our video html element and in an Object
     const canvasDisplaySize = { width: video.width, height: video.height }
+    // Set the size of our graphical elements from the faceAPI
     faceapi.matchDimensions(canvas, canvasDisplaySize)
 
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video,
-            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        const resizedDetections = faceapi.resizeResults(detections, canvasDisplaySize)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
+    // Every 100ms, do:
+    setInterval(
+        // anonymous async function:
+        async () => {
+            // Create an object that will asynchronously detect all the faces on the webcam and the the result
+            const detections = await faceapi.detectAllFaces(
+                video,
+                new faceapi.TinyFaceDetectorOptions()
+            ).withFaceLandmarks().withFaceExpressions()
+
+            // Reset canvas
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+            // Resize the Landmarks
+            const resizedDetections = faceapi.resizeResults(detections, canvasDisplaySize)
+            // Draw the rectangle of the face detected
+            faceapi.draw.drawDetections(canvas, resizedDetections)
+            // Draw Landmarks
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+            // Draw the name of the expression detected and is confident ratio
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        }, 100)
 })
